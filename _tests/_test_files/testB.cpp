2223143c262f9ae218e7ea3b4277840d8a77969f
Dylan Perez <dylanperez.h04@gmail.com>
2024-11-23 03:27:49 -0800

    Matched basic_test output for select commands with expected basic_test output (except for Sammuel L ., where I have Sammuel L. because I'm built different.

--- _tests/_test_files/testB.cpp
+++ _tests/_test_files/testB.cpp
@@ -1,22 +1,103 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <iomanip>
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <vector>
+
+//------------------------------------------------------------------------------------------
+//Files we are testing:
+#include "../../includes/sql/sql.h"
+
+
+//------------------------------------------------------------------------------------------
+
+
 using namespace std;
 
-bool test_stub(bool debug = false)
-{
-  if (debug){
-    cout << "testB:: test-sub() entering test_sub" << endl;
-  }
-  return true;
+const vector<string> command_list = {
+
+    //.................
+    //:Greater Than :
+    //.................
+    "select * from student where lname > Yang",
+    //. . . . . . . . . . . . . (Greater Than: Non-existing) . . . . . . . . . . . . . . . . . . . . .
+    "select * from student where age > 50",
+    //. . . . . . . . . . . . . (Greater than: last item) . . . . . . . . . . . . . . .
+    "select * from student where age > 53",
+    //. . . . . . . . . . . . . (Greater Than: Passed last item) . . . . . . . . . . . . . . . . . . . . .
+    "select * from student where age > 54",
+    //.................
+    //:Greater Equal :
+    //.................
+    "select * from student where lname >= Yang",
+    //. . . . . . (Greater Equal non-existing: ) . . . . . . . . . . .
+    "select * from employee where last >= Jack",
+    //.................
+    //:Less Than :
+    //.................
+    //. . . . . . . . . . . . . (Less Than: Non-existing) . . . . . . . . . . . . . . .
+    "select * from student where lname < Jackson",
+    //. . . . . . . . . . . . . (Less than: first item) . . . . . . . . . . . . . . . .
+    "select * from student where age < 20",
+    //. . . . . . . . . . . . . (Less Than: before first item) . . . . . . . . . . . . . . . . . . . . .
+    "select * from student where age < 19",
+    //.................
+    //:Less Equal :
+    //.................
+    "select * from student where lname <= Smith",
+    //. . . . . . (Less Equal non-existing: ) . . . . . . . . . . .
+    "select * from employee where last <= Peach"
+
+};
+
+const int COMMANDS = 1;
+
+bool test_sql_relational(bool debug = false) {
+
+    SQL sql;
+    Table t;
+
+    cout << endl
+        << endl;
+    for (int i = 0; i < COMMANDS; i++)
+    {
+        cout << "\n>" << command_list[i] << endl;
+        if (debug)
+            cout << sql.command(command_list[i]) << endl;
+        else
+            t = sql.command(command_list[i]);
+        cout << "basic_test: records selected: " << sql.select_recnos() << endl;
+    }
+
+    cout << "----- END TEST --------" << endl;
+    return true;
+
+    return true;
 }
 
-TEST(TEST_STUB, TestStub) {
-  
-  //EXPECT_EQ(0, <your individual test functions are called here>);
+TEST(TEST_SQL_RELATIONAL, TestRelational) {
 
-  EXPECT_EQ(1, test_stub(false));
+    EXPECT_EQ(1, test_sql_relational(false));
 }
+
+//bool test_stub(bool debug = false)
+//{
+//  if (debug){
+//    cout << "testB:: test-sub() entering test_sub" << endl;
+//  }
+//  return true;
+//}
+//
+//TEST(TEST_STUB, TestStub) {
+//  
+//  //EXPECT_EQ(0, <your individual test functions are called here>);
+//
+//  EXPECT_EQ(1, test_stub(false));
+//}
 
 
 
--- includes/sql/sql.cpp
+++ includes/sql/sql.cpp
@@ -2,7 +2,7 @@
 
 SQL::SQL() {}
 
-Table SQL::command(string query) {
+Table& SQL::command(string query) {
     const bool debug = false;
     Parser parsed_query(query.c_str());
     MMap<string, string>& ptree = parsed_query.parse_tree();
@@ -14,39 +14,45 @@ Table SQL::command(string query) {
     string cmd = ptree.get("command").front();
     string table_name = ptree.get("table_name").front();
     string file_name = table_name + ".bin";
+
     vector<string> fields;
+
+    Table to_return;
     if (cmd == "make") {
         fields = ptree.get("col");
         if (debug) cout << "command() : make command called. fields = " << fields << endl;
-        _table = Table(table_name, fields);
+        to_return = Table(table_name, fields);
     }
     else if (cmd == "insert") {
         if (!file_exists(file_name.c_str())) return _table;
         if (debug) cout << "command() : insert command called." << endl;
-        _table = Table(table_name);
+        to_return = Table(table_name);
         fields = ptree.get("values");
         if(debug) cout << "command() : cmd == insert | fields = " << fields << endl;
-        _table.insert_into(fields);
+        to_return.insert_into(fields);
     }
     else if (cmd == "select") {
         if (!file_exists(file_name.c_str())) return _table;
         _table = Table(table_name);
         fields = ptree.get("fields");
+        if (debug) cout << "command() : select | fields = " << fields << endl;
         if (!ptree.contains("where")) {
+            cout << "There is no condition" << endl;
             if (fields.front() == "*")
-                _table.select_all(_table.get_fields());
+                to_return = _table.select_all(_table.get_fields());
             else
-                _table.select_all(fields);
+                to_return = _table.select_all(fields);
         }
         else if(ptree.get("where").front() == "yes") {
             if (fields.front() == "*")
-                _table.select(_table.get_fields(), ptree.get("condition"));
+                to_return = _table.select(_table.get_fields(), ptree.get("condition"));
             else
-                _table.select(fields, ptree.get("condition"));
+                to_return = _table.select(fields, ptree.get("condition"));
         }
-    }
 
-    return _table;
+        cout << to_return << endl << endl;
+    }
+    return to_return;
 }
 
 vector<long> SQL::select_recnos()
