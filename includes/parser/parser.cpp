0fec0feebc158a176c9a013af0b25a730eb63e21
Dylan Perez <dylanperez.h04@gmail.com>
2024-11-28 23:59:38 -0800

    14

--- includes/parser/parser.cpp
+++ includes/parser/parser.cpp
@@ -141,7 +141,7 @@ bool Parser::get_parse_tree(Queue<string> q) {
         state = adj_table[state][key_type];
 
         if (state == -1)
-            assert(key_type != SYMBOL && prev_state != 12);   
+            assert(key_type != SYMBOL && prev_state != 14);   
 
         if (debug) cout << "get_parse_tree() : token = " << token << " | state = " << state << endl;
 
